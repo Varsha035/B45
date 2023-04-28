7ca234a3f3981567ebad4a7680063715b1a5920c
DELL <DELL@DESKTOP-J3U4BNV>
2023-04-28 11:02:05 +0530

    commit

---
+++ Day1/src/Dayone/Identifier.java
@@ -0,0 +1,22 @@
+package Dayone;
+
+public class Identifier {
+	public static void main(String[] args) {
+		
+		int fr =13;  
+		int number_2=23; 
+		
+		int numbar3=10;  
+		
+		int $numbar=20; 
+		
+		System.out.println("value of the numbar variable is : "+$numbar );
+		String studentsName="aniket";
+		System.out.println("studentsName");
+		
+		
+		
+
+	}
+
+}
