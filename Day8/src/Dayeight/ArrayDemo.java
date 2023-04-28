5a3bcd8373c65d80d8bf6184068fa1d8986f5ce1
DELL <DELL@DESKTOP-J3U4BNV>
2023-04-28 11:28:22 +0530

    commit

---
+++ Day8/src/Dayeight/ArrayDemo.java
@@ -0,0 +1,20 @@
+package Dayeight;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		String a[]= new String  [2];
+		a[0]= "swapnil";
+		a[1]= "pranv";
+	
+		
+
+		
+		for (int i=0; i<a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
