ba29256711718147076474ff5119471346bc3826
DELL <DELL@DESKTOP-J3U4BNV>
2023-04-28 11:24:28 +0530

    commit

---
+++ Day3/src/Daythree/Operator.java
@@ -0,0 +1,22 @@
+package Daythree;
+
+public class Operator {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int x=10;
+		System.out.println("a and b value before the operation: " + a + " " + b);
+		++a;   //a=11+1
+		int c=++a + b + a--;  
+		System.out.println("c value after the operation: " + c);
+		
+		int d=c++ + a + b--;  
+		System.out.println("d value after the operation: " + d);
+		System.out.println("a, b, c, d value after the operation: " + a + " " + c + " " + d );
+		  
+		x= (10==x) ? 1:0;
+		System.out.println(x);
+
+	}
+
+}
